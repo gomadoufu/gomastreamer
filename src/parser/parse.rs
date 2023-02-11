c4c934fb2868a0d72ad686e70987767c70845350
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-02-11 16:24:15 +0900

    Add comments

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -3,22 +3,29 @@ use clap::{Parser, ValueEnum};
 #[derive(Parser, Debug)]
 #[command(name = "gomastreamer")]
 #[command(author = "gomadoufu")]
-#[command(version = "0.1.0")]
+#[command(version)] // version is automatically detected from Cargo.toml
 #[command(about = "gomastreamer:\nThin Rust wrapper for gstreamer, for development, for myself", long_about = None)]
 pub struct Cli {
+    /// show information of devices
     #[arg(long)]
     pub show: bool,
+    /// host name of udpsink
     #[arg(default_value = "localhost")]
     pub host: String,
+    /// port number of udpsink
     #[arg(default_value = "8080")]
     pub port: i32,
+    /// source of video
     #[arg(value_enum, short, long, default_value = "test")]
     pub input: Input,
+    /// resolution of video
     #[arg(value_enum, short, long, default_value = "vga")]
     pub resolution: Resolution,
     //フォーマットは将来増えるかもしれないのでブーリアンにしない
+    /// format of video
     #[arg(value_enum, short, long, default_value = "vp8")]
     pub format: Format,
+    /// use hardware encode
     #[arg(long = "hardware")]
     pub hardware_encode: bool,
 }
